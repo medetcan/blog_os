8675a467b7865cce89bae12539d1688b1640f71b
medetcan <medetc.akkus@gmail.com>
2019-05-18 23:13:46 +0300

    avoided heavy compiler optimizations by using Volatile crate

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,7 @@
 
+use core::fmt;
+use volatile::Volatile;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,7 +45,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -62,22 +65,24 @@ impl Writer {
                 let row = BUFFER_HEIGHT - 1;
                 let column = self.column_position;
                 let color_code = self.color_code;
-                self.buffer.chars[row][column] = ScreenChar {
+                self.buffer.chars[row][column].write(ScreenChar {
                     ascii_character: byte,
                     color_code
-                };
+                });
                 self.column_position +=1;
             }
         }
     }
-    fn new_line(&mut self) {/*Not Implemented*/}
+    fn new_line(&mut self) {
+        // Not Implemented
+    }
     fn clear_buffer(&mut self) {
         for row in 0..BUFFER_HEIGHT {
             for column in 0..BUFFER_WIDTH {
-               self.buffer.chars[row][column] = ScreenChar {
+               self.buffer.chars[row][column].write(ScreenChar {
                    ascii_character: 0x00,
                    color_code: ColorCode::new(Color::White, Color::White)
-               }
+               });
             }
         }
     }
@@ -94,17 +99,24 @@ impl Writer {
     }
 }
 
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Red, Color::White),
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
-    writer.clear_buffer();
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
 
 
